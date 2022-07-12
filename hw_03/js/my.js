73545b70caea368419bade94b442f29e65b936dc
fartushnylex <fartushnylex69@gmail.com>
2022-07-12 11:49:42 +0300

    add zodiac simbol and fix console.log

--- hw_03/js/my.js
+++ hw_03/js/my.js
@@ -9,113 +9,129 @@ const yearOfBirth = Number(prompt("Введіть рік свого народж
 let age = CURRENT_YEAR - yearOfBirth;
 
 let zodiac;
+let simbol;
 
 switch (monthOfBirth) {
     case 1:
         if (dateOfBirth < 20) {
             zodiac = "Capricorn";
+            simbol = "♑";
         }
         else {
             zodiac = "Aquarius";
+            simbol = "♒";
         }
         break;
     case 2:
         if (dateOfBirth < 19) {
             zodiac = "Aquarius";
+            simbol = "♒";
         }
         else {
             zodiac = "Pisces";
+            simbol = "♓";
         }
         break;
     case 3:
         if (dateOfBirth < 21) {
-           zodiac = "Pisces"; 
+            zodiac = "Pisces";
+           simbol = "♓"; 
         }
         else {
             zodiac = "Aries";
+            simbol = "♈";
         }
         break;
     case 4:
         if (dateOfBirth < 20) {
-         zodiac = "Aries";   
+            zodiac = "Aries";
+            simbol = "♈";   
         }
         else {
             zodiac = "Taurus";
+            simbol = "♌";
         }
         break;
     case 5:
         if (dateOfBirth < 21) {
-           zodiac = "Taurus"; 
+            zodiac = "Taurus";
+            simbol = "♉";
         }
         else {
             zodiac = "Gemini";
+            simbol = "♊";
         }
         break;
     case 6:
         if (dateOfBirth < 21) {
-          zodiac = "Gemini";  
+            zodiac = "Gemini";
+            simbol = "♊";  
         }
         else {
             zodiac = "Cancer";
+            simbol = "♋";
         }
         break;
     case 7:
         if (dateOfBirth < 23) {
-           zodiac = "Cancer"; 
+            zodiac = "Cancer";
+            simbol = "♋"; 
         }
         else {
             zodiac = "Leo";
+            simbol = "♌";
         }
         break;
     case 8:
         if (dateOfBirth < 23) {
-            zodiac = "Leo"; 
+            zodiac = "Leo";
+            simbol = "♌";
         }
         else {
             zodiac = "Virgo";
+            simbol = "♍";
         }
         break;
     case 9:
         if (dateOfBirth < 23) {
             zodiac = "Virgo";
+            simbol = "♍";
         }
         else {
             zodiac = "Libra";
+            simbol = "♎";
         }
         break;
     case 10:
         if (dateOfBirth < 23) {
             zodiac = "Libra";
+            simbol = "♎";
         }
         else {
             zodiac = "Scorpio";
+            simbol = "♏";
         }
         break;
     case 11:
         if (dateOfBirth < 22) {
             zodiac = "Scorpio";
+            simbol = "♏";
         }
         else {
             zodiac = "Sagittarius";
+            simbol = "♐";
         }
         break;
     case 12:
         if (dateOfBirth < 22) {
             zodiac = "Sagittarius";
+            simbol = "♐";
         }
         else {
             zodiac = "Capricorn";
+            simbol = "♑";
         }
         break;
 }
 
-// let isLeapYear = yearOfBirth % 4 == 0 || yearOfBirth % 100 != 0 && yearOfBirth % 400 == 0 ? true : false; 
-
-// if (isLeapYear) {
-//     console.log("User Bio: " + firstName + " " + lastName + ", " + age + " years old " + "(leap year), " + zodiac + ";");
-// }
-// else {
-//     console.log("User Bio: " + firstName + " " + lastName + ", " + age + " years old, " + zodiac + ";");
-// }
-
- console.log("User Bio: " + firstName + " " + lastName + ", " + age + " years old " + (yearOfBirth % 4 == 0 || yearOfBirth % 100 != 0 && yearOfBirth % 400 == 0 ? "(leap year), " : ", ") + zodiac + ";");
+ console.log("User Bio: " + firstName + " " + lastName + ", " + age + " years old " + (yearOfBirth % 4 == 0 || yearOfBirth % 100 != 0 && yearOfBirth % 400 == 0 ? "(leap year), " : ", ") + zodiac + " " + simbol + ";");
