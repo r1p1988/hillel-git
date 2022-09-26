cdfafedf5fa68cc439bec5d6453e153faa4ac8a0
fartushnylex <fartushnylex69@gmail.com>
2022-09-26 11:53:42 +0300

    code refactoring

--- hw_22/js/main.js
+++ hw_22/js/main.js
@@ -1,44 +1,40 @@
 let jokesCategories = document.querySelector("#jokesCategories"),
     jokesList = document.querySelector("#jokesList"),
-    jokes = [],
     newCategories = [];
 
 const getFile = file => fetch(file)
     .then(data => data.ok ? data.json() : Promise.reject(data.statusText));
 
-getFile(`https://api.chucknorris.io/jokes/categories`)
+    getFile(`https://api.chucknorris.io/jokes/categories`)
     .then(data => {
         data.map(categories => newCategories.push(`<option value="${categories}">${categories}</option>`))
         jokesCategories.innerHTML = newCategories.join(" ");
     })
     .catch(err => console.log(err));
 
-
-jokesCategories.addEventListener(`change`, () => {
+    jokesCategories.addEventListener(`change`, () => {
     getFile(`https://api.chucknorris.io/jokes/random?category=${jokesCategories.value}`)
         .then(data => {
-            jokes.push(`<li>
-                <p>Category: <b>${data.categories}</b></p>
-                <p>${data.value}</p>
-                <button id="${data.categories}">Remove joke</button>
-                    </li>`)
-            jokesList.innerHTML = jokes.join(" ");
-            document.querySelector('option:checked').setAttribute("disabled", true);
-            // document.querySelector(`option[value=${jokesCategories.value}]`).setAttribute("disabled", true);
+            document
+                .querySelector(`option[value="${jokesCategories.value}"]`)
+                .setAttribute("disabled", true);
+
+            const li = document.createElement(`li`);
+            li.innerHTML = `<p>Category: <b>${data.categories[0]}</b></p>
+            <p>${data.value}</p>`;
+
+            const removeBtn = document.createElement(`button`);
+            removeBtn.innerHTML = `Remove joke`;
+            removeBtn.addEventListener(`click`, () => {
+                li.remove();
+                document
+                    .querySelector(`option[value="${data.categories[0]}"]`)
+                    .removeAttribute("disabled");
+            });
+
+            li.append(removeBtn);
+
+            jokesList.append(li);
         })
         .catch(err => console.log(err))
-})
-
-jokesList.addEventListener("click", function (event) {
-    let target = event.target;
-    if (target.tagName === 'BUTTON') {
-    const parent = target.closest("li");
-    if (parent !== null) {
-           const items = [...parent.parentElement.children];
-           const index = items.indexOf(parent);
-            jokes.splice(index, 1);
-       }
-    document.querySelector(`option[value=${target.id}]`).removeAttribute("disabled");
-    target.parentNode.remove();           
-    }
-})
+});
