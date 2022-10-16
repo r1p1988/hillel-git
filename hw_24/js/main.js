2e046b47e93b0b35c34945cd82c5642a8a472cea
fartushnylex <fartushnylex69@gmail.com>
2022-10-16 17:19:42 +0300

    refactoring code

--- hw_24/js/main.js
+++ hw_24/js/main.js
@@ -1,47 +1,40 @@
 const URL = `https://raw.githubusercontent.com/brightestsirius/Front-end-Pro-19/master/lesson_27/tesla.json`;
 const imgUrl = `https://mc-astro.github.io/tesla-roadster-colors/img`;
 
-const colorPicker = document.querySelector(".color-picker");
-const slideshowContainer = document.querySelector(".slideshow-container");
+const carImg = $(`#carImg`);
+const carImgTitle = $(`#carImgTitle`);
+const colorPicker = $(`.color-picker`);
+
+const renderColorBtns = (data) => {
+  $(data).each((index, item) => {
+    let spanColors = $(
+      `<span class="dot" style="background-color: ${item.color}"></span>`
+    );
+    spanColors.on(`click`, () => renderCar(item));
+    colorPicker.append(spanColors);
+  });
+};
+
+const renderCar = (item) => {
+  carImg.width("100%");
+  carImg.attr(
+    `src`,
+    imgUrl + `/${item.img}` + `.jpg`
+  );
+  carImg.attr(`alt`, item.title);
+
+  carImgTitle.html(item.title);
+  // carImgTitle.css(`color`, item.color);
+};
 
 $.ajax({
-    url : URL,
-    method: 'GET',
-        error: error => console.log(error)
-})
-    .done(() => {
-    let mySlides = document.createElement("div");
-    mySlides.className = "mySlides";
-
-    let title = document.createElement("div");
-    title.className = "text";
-                    
-    let imgColors = document.createElement("img");
-    $(imgColors).width("100%");
-
-    mySlides.append(imgColors);
-    mySlides.append(title);
-    slideshowContainer.append(mySlides);
-    }
-    )
-  .done(data=>{
-      $.each(JSON.parse(data), function (index, item) {
-        let spanColors = document.createElement("span");
-          $(spanColors).css('background', `${item.color}`);
-          $(spanColors).addClass(`dot`);
-
-          $(`.mySlides > img`).attr('src', imgUrl + `/${JSON.parse(data)[0].img}` + `.jpg`);
-          $(`.mySlides > img`).attr(`alt`, `${JSON.parse(data)[0].title}`);
-          $(`.text`).text(`${JSON.parse(data)[0].title}`);
-
-          colorPicker.append(spanColors);
-
-        $(spanColors).on(`click`, () => {
-            $(`.mySlides > img`).attr('src', imgUrl + `/${item.img}` + `.jpg`);
-            $(`.mySlides > img`).attr(`alt`, `${item.title}`);
-            $(`.text`).text(`${item.title}`);
-        })
-          
-      })
-  })
+  url: URL,
+  method: "GET",
+  success: (data) => {
+    data = JSON.parse(data);
+    renderColorBtns(data);
+    renderCar(data[0]);
+  },
+  error: (error) => console.log(error),
+});
     
